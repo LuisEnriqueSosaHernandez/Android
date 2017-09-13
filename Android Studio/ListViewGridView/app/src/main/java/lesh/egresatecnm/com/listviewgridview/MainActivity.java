cb1bd1eddf66b1927368ffbf0114b875eec56c6d
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2017-09-12 21:09:36 -0500

    Android

---
+++ Android Studio/ListViewGridView/app/src/main/java/lesh/egresatecnm/com/listviewgridview/MainActivity.java
@@ -0,0 +1,82 @@
+package lesh.egresatecnm.com.listviewgridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    List<String> names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView)findViewById(R.id.listView);
+        names=new ArrayList<String>();
+        names.add("Alejandro");
+        names.add("Fernando");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+        names.add("Yazmin");
+        names.add("Andrea");
+        names.add("Cryderman");
+        names.add("Coronel");
+
+        //  Forma visual en quese mostrarann los datos
+        //ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,names);
+        //enlazamos el adaptador con nuestrolist view
+        //listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked "+names.get(position), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        //Enlazamos con nuestro adaptador personalizado
+        MyAdapter myAdapter=new MyAdapter(this,R.layout.listview,names);
+        listView.setAdapter(myAdapter);
+    }
+}
---
+++ Android Studio/ListViewGridView/app/src/main/java/lesh/egresatecnm/com/listviewgridview/MyAdapter.java
@@ -0,0 +1,69 @@
+package lesh.egresatecnm.com.listviewgridview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MyAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<String> names;
+    public MyAdapter(Context context
+    ,int layout,List<String>names){
+        this.context=context;
+        this.layout=layout;
+        this.names=names;
+
+    }
+
+    @Override
+    public int getCount() {
+
+        return this.names.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return this.names.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //ViewHolderPattern
+        ViewHolder holder;
+        if(convertView==null){
+            //Inflamos la vista que nos ha llegado con nuestro layout personalizado
+            LayoutInflater layoutInflater=LayoutInflater.from(this.context);
+            convertView=layoutInflater.inflate(R.layout.listview,null);
+            holder=new ViewHolder();
+            //Referenciamos el evento a modificar y lo rellenamos
+            holder.nameTextView=(TextView)convertView.findViewById(R.id.nombres);
+            convertView.setTag(holder);
+        }else{
+            holder=(ViewHolder)convertView.getTag();
+        }
+
+        //Traemos el valor actual dependiente de la posicion
+        String currentName=names.get(position);
+        holder.nameTextView.setText(currentName);
+        //devolvemos inflados y modificados los datos lel xD jejjej Baia Baia
+        return convertView;
+    }
+    static class ViewHolder {
+        private TextView nameTextView;
+    }
+}
