c29c0f1aea56bec54339aedb25a01768f537c902
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-03-12 18:20:46 -0600

    Splash

---
+++ Android Studio/Zona Verde/ZonaVerde/app/src/main/java/iolesh/github/luisenriquesosahernandez/zonaverde/SplashActivity.java
@@ -0,0 +1,16 @@
+package iolesh.github.luisenriquesosahernandez.zonaverde;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent=new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
