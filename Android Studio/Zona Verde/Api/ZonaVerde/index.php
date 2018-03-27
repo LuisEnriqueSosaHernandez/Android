6d40d38e8361a31e26ebfddeff3cfe956d7b87db
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-03-26 18:00:28 -0600

    Api

--- Android Studio/Zona Verde/Api/ZonaVerde/index.php
+++ Android Studio/Zona Verde/Api/ZonaVerde/index.php
@@ -4,12 +4,22 @@ $app=new \Slim\Slim();
 $db=new mysqli("localhost","root","","Zona Verde");
 mysqli_set_charset($db, "utf8");
 
-$app->get("/estados",function () use($db,$app){
-    $query=$db->query("SELECT * FROM estados;");
+$app->get("/Estados",function () use($db,$app){
+    $query=$db->query("SELECT * FROM Estados;");
     $estados= array();
     while($fila=$query->fetch_assoc()){
        $estados[]=$fila;
     }
     echo json_encode($estados);
 });
+
+$app->get("/Municipios/:IdEstado",function ($IdEstado) use($db,$app){
+    $query=$db->query("SELECT * FROM Municipios WHERE IdEstado=".$IdEstado.";");
+    $municipios= array();
+    while($fila=$query->fetch_assoc()){
+       $municipios[]=$fila;
+    }
+    echo json_encode($municipios);
+});
+
 $app->run();
