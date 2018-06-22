3167a3d6f8ffb722f352dc24d76cfdbc3e7b6076
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-06-22 15:30:56 -0500

    FloatingButtom

--- Android Studio/Board/app/src/main/java/com/example/lesh/board/Activities/BoardActivity.java
+++ Android Studio/Board/app/src/main/java/com/example/lesh/board/Activities/BoardActivity.java
@@ -1,15 +1,25 @@
 package com.example.lesh.board.Activities;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.lesh.board.R;
 
 public class BoardActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+        fab=findViewById(R.id.fabAddBoard);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
