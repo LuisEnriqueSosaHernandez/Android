aa4338233367e142ddd8a747e9ef65ad9f08c179
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-06-26 15:31:20 -0500

    Adapter

--- Android Studio/Board/app/src/main/java/com/example/lesh/board/Activities/BoardActivity.java
+++ Android Studio/Board/app/src/main/java/com/example/lesh/board/Activities/BoardActivity.java
@@ -11,46 +11,61 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.lesh.board.Models.Board;
 import com.example.lesh.board.R;
 
+import io.realm.Realm;
+
 public class BoardActivity extends AppCompatActivity {
+    private Realm realm;
     private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Bb realm
+        realm=Realm.getDefaultInstance();
         setContentView(R.layout.activity_board);
         fab = findViewById(R.id.fabAddBoard);
-        showAlertForCreatingBoard("title","message");
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAlertForCreatingBoard("Add new Board", "Type a name for your new board");
+            }
+        });
+    }
+//Crud actions
+    private void createNewBoard(String boardName) {
+        realm.beginTransaction();
+        Board board=new Board(boardName);
+        realm.copyToRealm(board);
+        realm.commitTransaction();
     }
 
-    private void showAlertForCreatingBoard(String title,String message){
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+    //Dialogs
+    private void showAlertForCreatingBoard(String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        if(!TextUtils.isEmpty(title))builder.setTitle(title);
-        if(!TextUtils.isEmpty(message))builder.setMessage(message);
+        if (!TextUtils.isEmpty(title)) builder.setTitle(title);
+        if (!TextUtils.isEmpty(message)) builder.setMessage(message);
 
-        View viewInflated=LayoutInflater.from(this).inflate(R.layout.dialog_create_board,null);
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board, null);
         builder.setView(viewInflated);
 
-        final EditText input=viewInflated.findViewById(R.id.editTextNewBoard);
+        final EditText input = viewInflated.findViewById(R.id.editTextNewBoard);
 
         builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                String boardName=input.getText().toString().trim();
-                if(boardName.length()>0){
+                String boardName = input.getText().toString().trim();
+                if (boardName.length() > 0) {
                     createNewBoard(boardName);
-                }else{
+                } else {
                     Toast.makeText(getApplicationContext(), "The name is required to create a new Board", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        AlertDialog dialog=builder.create();
+        AlertDialog dialog = builder.create();
         dialog.show();
-    }
-
-    private void createNewBoard(String boardName) {
-
     }
 }
---
+++ Android Studio/Board/app/src/main/java/com/example/lesh/board/Adapters/BoardAdapter.java
@@ -0,0 +1,70 @@
+package com.example.lesh.board.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.lesh.board.Models.Board;
+import com.example.lesh.board.R;
+
+import java.util.List;
+
+public class BoardAdapter extends BaseAdapter{
+
+    private Context context;
+    private List<Board>list;
+    private int layout;
+
+    public BoardAdapter(Context context,List<Board>boards,int layout){
+        this.context=context;
+        this.list=boards;
+        this.layout=layout;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Board getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int id) {
+        return id;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        ViewHolder vh;
+
+        if(convertView==null){
+            convertView= LayoutInflater.from(context).inflate(layout,null);
+            vh=new ViewHolder();
+            vh.title=convertView.findViewById(R.id.textViewBoardTitle);
+            vh.notes=convertView.findViewById(R.id.textViewBoardNotes);
+            vh.createdAt=convertView.findViewById(R.id.textViewBoardDate);
+            convertView.setTag(vh);
+        }else{
+            vh=(ViewHolder)convertView.getTag();
+        }
+        Board board=list.get(position);
+        vh.title.setText(board.getTitle());
+        int numberOfNotes=board.getNotes().size();
+        String textForNotes=(numberOfNotes==1)?numberOfNotes+" Note":numberOfNotes+" Notes";
+        vh.notes.setText(textForNotes);
+        vh.createdAt.setText(board.getCreatedAt().toString());
+        return null;
+    }
+
+    public class ViewHolder{
+        TextView title;
+        TextView notes;
+        TextView createdAt;
+    }
+}
