0e6c786526c268b6c8ba9f0b6dbc29da1467f3e4
LuisEnriqueSosaHernandez <kiquesasuke@gmail.com>
2018-07-02 13:30:42 -0500

    Maps

---
+++ Android Studio/Maps/app/src/main/java/com/example/lesh/maps/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.example.lesh.maps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng veracruz = new LatLng(19.2059701, -96.195701);
+        mMap.addMarker(new MarkerOptions().position(veracruz).title("Hola desde Veracruz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(veracruz));
+    }
+}
